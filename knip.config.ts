cb24fa72cca07b0f1b073faab592e44980b6c703
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2024-09-18 16:39:39 +0100

    feat: dsadfasdfsadf

--- knip.config.ts
+++ knip.config.ts
@@ -3,6 +3,7 @@ import type { KnipConfig } from 'knip'
 const config: KnipConfig = {
   entry: ['src/**/*'],
   ignore: ['postcss.config.mjs', 'postcss-load-config'],
+  //
 }
 
 export default config
